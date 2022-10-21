b1a03d2eaa304ed4c596df77fac87213b0490b3a
kaushaltheeG <kaushalsk09@gmail.com>
2022-10-21 16:10:07 -0700

    got conditionals for valid moves

--- knights_travails/01_knight_path_finder.rb
+++ knights_travails/01_knight_path_finder.rb
@@ -16,12 +16,19 @@ class KnightPathFinder
     # o
     # x
     # x x
-    # 
-    # 
-    # 
+    # #edge cases
+    #   Corners: [0,0], [7,0], [0,7], [7,7]
+    #   1 space coners: [(x - 1 == 0), (y + 1 == 7)], [(x - 1 == 0), (y - 1 == 0)], [(x + 1 == 7), (y + 1 == 7)], [(x + 1 == 7), (y - 1 == 0)]
+    #   1 space only (ranges):  1. [[(x - 1 == 0) || (x + 1 == 7)] && (2 < y < 6)] 2.[[(y - 1 == 0) || (y + 1 == 7)] && (2 < x < 6)]
+
     #     
     def self.valid_moves(pos)
-
+        #full range 
+        #move 
+        (0...pos_row)
+            #move up one 
+            ()
+        (pos_row+1..pos_row+2)
     end
 
 

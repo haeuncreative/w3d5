820f1c8a0b7d9c0a7aaef51e72fea7d532e9370b
kaushaltheeG <kaushalsk09@gmail.com>
2022-10-21 14:16:11 -0700

    dfs done

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class PolyTreeNode
     attr_reader :value, :parent, :children
 
@@ -21,7 +23,35 @@ class PolyTreeNode
             parent_node.children << self
         
         end
-
     end
 
+    def add_child(child_node)
+        child_node.parent = self
+    end 
+
+    def remove_child(child_node)
+        if child_node.parent.nil?
+            raise "Not a child"
+        else 
+            child_node.parent = nil
+        end 
+    end
+
+    def inspect
+        { 'value' => @value, 'children' => @children }.inspect
+    end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+        return nil if self.children.empty?
+        self.children.each do |child| 
+            child_dfs = child.dfs(target_value)
+            return child_dfs unless child_dfs.nil?
+        end 
+        nil
+    end 
+
+    
+
 end
+

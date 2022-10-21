b37ec1c433773f5034b9ca91b072d9d034ded80e
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2022-10-20 23:26:15 -0700

    finished DIY ADT

--- DIY_ADT_exer.rb
+++ DIY_ADT_exer.rb
@@ -19,5 +19,57 @@ class Stack
     def peek
         @newest
     end
+end
+
+class Queue
+    def initialize
+        @data = Array.new
+
+    end
+
+    def enqueue(ele)
+        @data << ele            
+    end
+
+    def dequeue
+        @data.shift
+    end
+
+    def peek
+        @data[0]
+    end
+
+end
+
+class Map
+    def initialize
+        @data = Array.new
+    end
+
+    def set(key, val)
+        @data << [key, val]
+    end
+
+    def get(key)
+        @data.each do |sub|
+            if sub[0] == key
+                return sub[1]
+            end
+        end
+        return nil
+    end
+
+    def delete(key)
+        @data.each do |sub|
+            if sub[0] == key
+                @data.delete(sub)
+            end
+        end
+        return nil
+    end
+
+    def show
+        @data
+    end
 
 end

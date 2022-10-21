e84c660a758475f6307519c82e7e706ed31d8418
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2022-10-21 11:50:31 -0700

    finished parent method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -7,4 +7,21 @@ class PolyTreeNode
         @children = Array.new
     end
 
+    def parent=(parent_node)
+        if parent_node == nil
+            @parent = nil
+        elsif @parent.nil?
+            @parent = parent_node
+                if !parent_node.children.include?(self)
+                    parent_node.children << self
+                end
+        elsif !@parent.nil?
+            @parent.children.delete(self)
+            @parent = parent_node
+            parent_node.children << self
+        
+        end
+
+    end
+
 end
